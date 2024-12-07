2fc44a62b6e3944c56a46991559422d5719eb855
Zeke-MA <gregorios.tzikas@gmail.com>
2024-12-06 23:35:43 -0500

    Added input, ingestion, starting point

--- 2024/Day6/Day6.py
+++ 2024/Day6/Day6.py
@@ -1,3 +1,20 @@
+MOVEMENT = ["^", ">", "v", "<"] 
+
 def file_read():
     with open("input.txt", "r") as f:
-        return f.readlines()
+        return [row.strip("\n") for row in f.readlines()]
+    
+def find_starting_point(rows):
+    for i in range(len(rows)):
+        if "^" in rows[i]:
+            row_idx = i
+            col_idx = rows[i].index("^")
+            break
+    return row_idx, col_idx
+
+def main():
+    input = file_read()
+    print(input)
+    print(find_starting_point(input))
+
+main()
